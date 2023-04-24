385767d352ed767e96da67cfb093d8ee6c79702c
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-04-24 12:00:49 +0530

    Added Cart animation by dynamically adding CSS classes

--- src/components/HeaderCartButton.js
+++ src/components/HeaderCartButton.js
@@ -1,18 +1,33 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "./Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../store/cart-context";
 
 export default function HeaderCartButton(props) {
   const cartContent = useContext(CartContext);
-
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
   console.log(cartContent);
+  const btnClasses = `${styles.button} ${isBtnHighlighted ? styles.bump : ""}`;
+  // To add 'bump' animation class to Btn for 300ms & then remove it
+  useEffect(() => {
+    if (cartContent.items.length === 0) return;
+    // Class added
+    setIsBtnHighlighted(true);
+    // Class removed after 300ms
+    const timeoutBtnClass = setTimeout(() => {
+      setIsBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timeoutBtnClass);
+    };
+  }, [cartContent.items]);
+
   const numberOfItems = cartContent.items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
